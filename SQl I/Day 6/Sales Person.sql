568f532e4677ab749063670b3864449d1cfd3d70
Sajal1302 <sajalgupta824@gmail.com>
2022-09-08 23:28:22 +0530

    Day 6 completed

---
+++ SQl I/Day 6/Sales Person.sql
@@ -0,0 +1,4 @@
+select salesperson.name from salesperson 
+where salesperson.sales_id not in 
+(select orders.sales_id from orders inner join company 
+on company.com_id=orders.com_id and company.name='red');
